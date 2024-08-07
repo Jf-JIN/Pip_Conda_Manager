70e3ca8f27c0bba96d4b2a9963f81f8131669381
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-08-07 03:02:28 +0200

    更新pip安装，更新，单行命令的类

--- pyscript/QThread_pip.py
+++ pyscript/QThread_pip.py
@@ -6,6 +6,8 @@ import os
 import threading
 import subprocess
 
+from PyQt5.QtWidgets import QTreeWidgetItem
+
 
 class QThread_pip_update(QThread):
     signal_textbrowser = pyqtSignal(str)
@@ -53,7 +55,7 @@ class QThread_pip_update(QThread):
 
 
 class QThread_pip_install(QThread):
-    output_to_textbrowser = pyqtSignal(str)
+    signal_textbrowser = pyqtSignal(str)
 
     def __init__(self, python_folder_path, python_version, install_flag, config_list):
         super().__init__()
@@ -68,8 +70,8 @@ class QThread_pip_install(QThread):
             if output_line == '' and self.process.poll() is not None:
                 break
             if output_line:
-                self.output_to_textbrowser.emit(output_line.strip())
-        self.output_to_textbrowser.emit(f"\n__________  {item}  已完成操作 __________\n\n")
+                self.signal_textbrowser.emit(output_line.strip())
+        self.signal_textbrowser.emit(f"\n__________  {item}  已完成操作 __________\n\n")
 
     def run(self):
         for item in self.config_list:
@@ -82,4 +84,76 @@ class QThread_pip_install(QThread):
                 self.thread = threading.Thread(target=self.read_output(item))
                 self.thread.start()
             except subprocess.CalledProcessError as e:
-                self.textBrowser.append(f"__________ {item} 操作失败: {e} __________\n")
+                self.signal_textbrowser.emit(f"__________ {item} 操作失败: {e} __________\n")
+
+
+class QThread_Single_Command(QThread):
+    signal_textbrowser = pyqtSignal(str)
+    signal_finished = pyqtSignal()
+
+    def __init__(self, command, env_path_list=None):
+        super().__init__()
+        self.ori_command = command
+        self.command = command
+        if env_path_list:
+            self.command = env_path_list[1] + ' ' + self.command
+            if 'conda' in env_path_list[0]:
+                conda_env_name = env_path_list[0].split(')')[1]
+                self.command = f'conda activate {conda_env_name} && ' + self.ori_command
+        self.command = f'echo Y | {self.command}'
+
+    def read_output(self):
+        while True:
+            output_line = self.process.stdout.readline()
+            if output_line == '' and self.process.poll() is not None:
+                break
+            if output_line:
+                self.signal_textbrowser.emit(output_line.strip())
+                # print(repr(output_line))
+        self.signal_textbrowser.emit(f"\n__________  已完成命令操作 __________\n\n")
+        self.signal_finished.emit()
+
+    def run(self):
+        try:
+            # print(self.command)
+            self.process = subprocess.Popen(self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.thread_read = threading.Thread(target=self.read_output)
+            self.thread_read.start()
+        except subprocess.CalledProcessError as e:
+            self.signal_textbrowser.emit(f"__________ 操作失败: {e} __________\n")
+
+
+class QThread_Pipdeptree(QThread):
+    signal_deptree_line = pyqtSignal(str)
+    signal_textbrowser = pyqtSignal(str)
+    signal_finished = pyqtSignal()
+
+    def __init__(self, tree_item):
+        super().__init__()
+        self.tree_item: QTreeWidgetItem = tree_item
+        text_0 = self.tree_item.text(0)
+        text_1 = self.tree_item.text(1)
+        if text_0.startswith('(conda'):
+            conda_name = text_0.split(')')[1]
+            self.command = f'conda activate {conda_name} && pipdeptree'
+        else:
+            python_path = text_1
+            self.command = f'{python_path} -m pipdeptree'
+
+    def read_output(self):
+        while True:
+            output_line = self.process.stdout.readline()
+            if output_line == '' and self.process.poll() is not None:
+                break
+            if output_line:
+                self.signal_deptree_line.emit(output_line)
+                # print(repr(output_line))
+        self.signal_finished.emit()
+
+    def run(self):
+        try:
+            self.process = subprocess.Popen(self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.thread_read = threading.Thread(target=self.read_output)
+            self.thread_read.start()
+        except subprocess.CalledProcessError as e:
+            self.signal_textbrowser.emit(f"__________ 操作失败: {e} __________\n")
