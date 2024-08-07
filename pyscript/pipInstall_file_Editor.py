98949f85c1de5d871cb71be2f6c3e5843e3576fa
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-08-07 03:03:54 +0200

    更新pipInstall编辑器

--- pyscript/pipInstall_file_Editor.py
+++ pyscript/pipInstall_file_Editor.py
@@ -1,7 +1,7 @@
 
 
 from PyQt5.QtGui import QCloseEvent, QKeySequence
-from PyQt5.QtWidgets import QDialog, QTextEdit, QToolBar, QAction, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QDialog, QTextEdit, QVBoxLayout, QMessageBox
 from PyQt5.QtCore import Qt
 
 
