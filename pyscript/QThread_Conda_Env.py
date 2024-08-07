265954c0533c99b6c5928ed0ed226d17aa4e624c
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-08-07 03:03:04 +0200

    增加对conda其他报错提示的过滤

--- pyscript/QThread_Conda_Env.py
+++ pyscript/QThread_Conda_Env.py
@@ -18,7 +18,13 @@ class QThread_Conda_Get_Env_List(QThread):
         try:
             result = subprocess.Popen('conda env list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, creationflags=subprocess.CREATE_NO_WINDOW)
             output = result.stdout.read().strip()
-            lines = output.split('\n')
+            lines: list = output.split('\n')
+            # 过滤 Conda 可能的报错部分
+            for index, item in enumerate(lines):
+                item: str
+                if item.startswith('#'):
+                    lines = lines[index:]
+                    break
             # 获取环境列表 conda_env_list, 列表中的每一个元素中 [0]是环境名，[1]是环境地址
             conda_env_list: list = [line.split() for line in lines if not line.startswith('#')]
             self.signal_conda_env_list.emit(conda_env_list)
